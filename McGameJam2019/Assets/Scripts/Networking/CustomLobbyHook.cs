6cb06f2c0e15956bd05fedc8fd9b0977ce874153
MEPC\Divit <dsharma5095@gmail.com>
2019-03-16 16:55:38 -0400

    Now added lobby

---
+++ McGameJam2019/Assets/Scripts/Networking/CustomLobbyHook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+enum CharacterClass
+{
+    Legend = 0,
+
+}
+
+public class CustomLobbyHook : Prototype.NetworkLobby.LobbyHook
+{
+    [SerializeField]
+    int prefabIndex = 0;
+
+    [SerializeField]
+    GameObject[] playerPrefabs;
+
+    void OnValidate()
+    {
+        //GetComponent<NetworkLobbyManager>().gamePlayerPrefab = playerPrefabs[prefabIndex];
+    }
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        //gamePlayer = Instantiate(playerPrefab, gamePlayer.transform.position, Quaternion.identity);
+        //NetworkServer.Spawn(playerPrefab);
+    }
+}
---
+++ McGameJam2019/Assets/Scripts/Networking/TypedPlayerSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypedPlayerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    Sprite healerSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<Healer>();
+        GetComponent<SpriteRenderer>().sprite = healerSprite;
+    }
+
+}
