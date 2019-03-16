c04b4e9ef1bfd2631cfdfd10832e2cadd40f0de2
jacob-hochstrasser <35405376+jacob-hochstrasser@users.noreply.github.com>
2019-03-16 10:53:54 -0400

    Completed basic movement
    Added basic character movement. Also did some code simplification/cleanup and removed all unnecessary assets.

--- McGameJam2019/Assets/Scripts/Platformer2DUserControl.cs
+++ McGameJam2019/Assets/Scripts/Platformer2DUserControl.cs
@@ -7,39 +7,25 @@ namespace UnityStandardAssets._2D
     public class Platformer2DUserControl : MonoBehaviour
     {
         private PlatformerCharacter2D m_Character;
-        private bool m_Jump;
-
-
+        
         private void Awake()
         {
             m_Character = GetComponent<PlatformerCharacter2D>();
         }
 
-
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = UnityEngine.Input.GetButtonDown("Jump");
-            }
-        }
-
-
         private void FixedUpdate()
         {
             // Read the inputs.
-            bool crouch = UnityEngine.Input.GetKey(KeyCode.LeftControl);
-            float h = UnityEngine.Input.GetAxis("Horizontal");
-            float v = UnityEngine.Input.GetAxis("Vertical");
-            float a1 = UnityEngine.Input.GetAxis("Fire1");
-            float a2 = UnityEngine.Input.GetAxis("Fire2");
-            float a3 = UnityEngine.Input.GetAxis("Fire3");
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            float a1 = Input.GetAxis("Fire1");
+            float a2 = Input.GetAxis("Fire2");
+            float a3 = Input.GetAxis("Fire3");
 
 
             // Pass all parameters to the character control script.
-            m_Character.Move(h, v, crouch, m_Jump);
-            m_Jump = false;
+            m_Character.Move(h, v, crouch);
         }
     }
 }
