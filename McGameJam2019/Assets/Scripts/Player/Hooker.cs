7bf10acf534d0deb9ea30375467e5a2ad5b43e05
q349wang <q349wang@edu.uwaterloo.ca>
2019-03-16 23:28:20 -0400

    Added spawns and started hooker

---
+++ McGameJam2019/Assets/Scripts/Player/Hooker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hooker : BasePlayer
+{
+    public Sprite hookerSprite;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        movementSpeed = 5;
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
