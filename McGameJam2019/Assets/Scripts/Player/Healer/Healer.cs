39a209734abb77c414f6c01101d5a5dfd16124f8
jacob-hochstrasser <35405376+jacob-hochstrasser@users.noreply.github.com>
2019-03-17 00:17:36 -0400

    Added resurrection

--- McGameJam2019/Assets/Scripts/Player/Healer/Healer.cs
+++ McGameJam2019/Assets/Scripts/Player/Healer/Healer.cs
@@ -12,8 +12,7 @@ public class Healer : BasePlayer
     // Start is called before the first frame update
     protected override void Start()
     {
-        fixedAbilities = new string[] { "HealObject" };
-        // gameObject.AddComponent<Heal>();
+        fixedAbilities = new string[] { "HealerAbilities" };
         movementSpeed = 6;
         base.Start();
     }
